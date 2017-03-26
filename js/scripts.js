24754955d46d0f03ce7c796f8c083045c97a3d58
Krzysztof Barański <Krzysztof Baraski>
2017-03-27 00:14:40 +0200

    poprawione

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,9 @@ var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
 var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
 var allNames = femaleNames.concat(maleNames);
 console.log(allNames);
-var newName ='Marian'
+var newName = 'Marian';
 
 if (allNames.indexOf(newName) === -1) {
-  var allName = allNames.push(newName)
+  allNames.push(newName);
   console.log(allNames);
 }
